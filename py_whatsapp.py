70acb8160a038297811705f0c97b943a2f5e2a6b
Omkar Kadam <omkarkadam-inft@atharvacoe.ac.in>
2021-08-10 10:58:04 +0530

    Update py_whatsapp.py

--- py_whatsapp.py
+++ py_whatsapp.py
@@ -1,20 +1,31 @@
 from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get('https://web.whatsapp.com/')
-
-name = input('Enter the name of user or group : ')
-msg = input('Enter your message : ')
-count = int(input('Enter the count : '))
-
-input('Enter anything after scanning QR code')
-
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
-
-msg_box = driver.find_element_by_class_name('_2S1VP')
-
-for i in range(count):
-    msg_box.send_keys(msg)
-    button = driver.find_element_by_class_name('_35EW6')
-    button.click()
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+  
+# Replace below path with the absolute path
+# to chromedriver in your computer
+driver = webdriver.Chrome('/home/saket/Downloads/chromedriver')
+  
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+  
+# Replace 'Friend's Name' with the name of your friend 
+# or the name of a group 
+target = '"Friend\'s Name"'
+  
+# Replace the below string with your own message
+string = "Message sent using Python!!!"
+  
+x_arg = '//span[contains(@title,' + target + ')]'
+group_title = wait.until(EC.presence_of_element_located((
+    By.XPATH, x_arg)))
+group_title.click()
+inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
+input_box = wait.until(EC.presence_of_element_located((
+    By.XPATH, inp_xpath)))
+for i in range(100):
+    input_box.send_keys(string + Keys.ENTER)
+    time.sleep(1)
